8556f956d2eea24047d3e4684bcbad80b1a4df03
Akshay-Borole <88107265+Akshay-Borole@users.noreply.github.com>
2021-09-06 16:36:49 +0530

    Create home.js

---
+++ frontend/component/home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div >
+            <p><center><b>WELCOME EVERYONE</b></center></p>
+            <h2><center> Home Page</center></h2>{"\n"}{"\n"}<br/><br/>
+           {"\n"}{"\n"}<p><center><b>PREDICTO</b> is a software that helps in making predictions on stocks using strategies.</center></p>
+
+
+               <br/> {"\n"}<p>  Our application makes predictions based on strategies which are implemented using the analysis of historical and live data of stocks for effective investment decisions.<br/>
+                      <br/> Prediction of stocks by implementing strategies includes reading the charts and using statistical figures to identify the trends in the stock market.</p>
+
+               <br/> {"\n"}<p>As Stock Markets are evolving and becoming complex, it is difficult for the investor to get the efficient and reliable stock information of public companies in 
+                   
+                           {"\n"}less time and they face problems while making the correct investment decision.</p>
+        </div>
+    )
+ }
+
+export default Home
