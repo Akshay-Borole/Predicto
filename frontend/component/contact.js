e53837642f6b034408736787c06fc40fdeae28fa
Akshay-Borole <88107265+Akshay-Borole@users.noreply.github.com>
2021-09-06 16:34:47 +0530

    Create contact.js

---
+++ frontend/component/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+    return (
+        <div className='contact'>
+            <h1><center>Contact Us</center></h1>
+            <p><center>Department of Scientific Computing, Modeling and Simulation </center></p>
+            <center> <a href ="http://www.unipune.ac.in/">Savitribai Phule Pune University</a></center>
+        </div>
+    )
+}
+
+export default Contact
