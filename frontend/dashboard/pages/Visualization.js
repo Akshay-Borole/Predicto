1d8f0e86b380212740548eac0bd4998dba56ca33
bhushankirve <80456309+bhushankirve@users.noreply.github.com>
2021-09-13 15:27:24 +0530

    Create Visualization.js

---
+++ frontend/dashboard/pages/Visualization.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const Visualization = () => {
+  return (
+    <div className='home'>
+      <h1>Visualization</h1>
+    </div>
+  );
+};
+
+export const Graphicalrepresentation = () => {
+  return (
+    <div className='home'>
+      <h1>Graphical Representation</h1>
+    </div>
+  );
+};
+
+export const Numericalrepresentation = () => {
+  return (
+    <div className='home'>
+      <h1>Numerical Representation</h1>
+    </div>
+  );
+};
+
