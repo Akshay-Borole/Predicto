457d08047a64cef0e45b8d3eead16978b3e56df9
bhushankirve <80456309+bhushankirve@users.noreply.github.com>
2021-09-13 15:26:09 +0530

    Create Strategy.js

---
+++ frontend/dashboard/pages/Strategy.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export const Strategy = () => {
+  return (
+    <div className='home'>
+      <h1>Strategy</h1>
+    </div>
+  );
+};
+
+export const Makestrategy = () => {
+  return (
+    <div className='home'>
+      <h1>Make Strategy</h1>
+    </div>
+  );
+};
+
+export const Comparestrategy = () => {
+  return (
+    <div className='home'>
+      <h1>Compare Strategy</h1>
+    </div>
+  );
+};
+
+export const Buystrategy = () => {
+  return (
+    <div className='home'>
+      <h1>Buy Strategy</h1>
+    </div>
+  );
+};
